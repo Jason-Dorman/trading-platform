26f6a9cb0a48370d6d34a9f8f90ab08ad1ae8d18
Jason Dorman <jasondorman5@gmail.com>
2020-12-03 18:58:35 -0500

    updating slopes

--- app/slope_example.py
+++ app/slope_example.py
@@ -2,6 +2,7 @@ import yfinance as yf
 import numpy as np
 import datetime as dt
 import statsmodels.api as sm
+import matplotlib.pyplot as plt
 
 ticker = 'AAPL'
 start_date = dt.datetime.today() - dt.timedelta(365)
@@ -10,15 +11,16 @@ end_date = dt.datetime.today()
 data = yf.download(ticker, start_date, end_date)
 
 
-def consecutive_slope(ser, n):
+def consecutive_slope(ser,
+                      n):  # ser is a numpy series can change to df if needed/wanted. n is number of consectuvie points of adjusted close can change to higher number for longer view period
     # calculates slope of regression line for n consecutive points
-    ser = (ser - ser.min()) / (ser.max() - ser.min())
-    x = np.array(range(len(ser)))
-    x = (x - x.min()) / (x.max() - x.min())
     slopes = [i * 0 for i in range(n - 1)]
     for i in range(n, len(ser) + 1):
-        y_scaled = ser[i - n:1]
-        x_scaled = x[:n]
+        # range never includes last value so you add one to include that last value
+        y = ser[i - n:i]
+        X = np.array(range(n))
+        y_scaled = (y - y.min()) / (y.max() - y.min())
+        x_scaled = (X - X.min()) / (X.max() - X.min())
         x_scaled = sm.add_constant(x_scaled)
         model = sm.OLS(y_scaled, x_scaled)
         results = model.fit()
@@ -42,5 +44,7 @@ def previous_slope(ser, n):
     return np.array(slope_angle)
 
 
-data['close_slope'] = consecutive_slope(data['Adj Close'], 5)
-print(data)
+df = data.copy()
+df['slopes'] = consecutive_slope(data['Adj Close'], 5)
+
+plot = df.iloc[:, [4, 6]].plot(subplots=True, layout=(2, 1))
